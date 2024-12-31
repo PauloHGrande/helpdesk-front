12983ded22481a6d3dcc409c5aae459540020b91
PauloHGrande <pauloav2013@gmail.com>
2024-12-31 11:14:12 -0300

    Botões de Ação para Técnico

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Tecnico } from 'src/app/models/tecnico';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,12 +11,47 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class TecnicoCreateComponent {
 
+  tecnico: Tecnico = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, [Validators.minLength(3), Validators.maxLength(50)]);
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
+  constructor(private service: TecnicoService, private toast: ToastrService) {}
+
   validaCampos(): boolean {
     return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe(resposta => {
+      this.toast.success('Técnico cadastrado com sucesso', 'Cadastro');
+    }, ex => {
+      console.log(ex);
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  addPerfil(perfil: any): void {
+    if(this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
   }
 }
--- src/app/models/tecnico.ts
+++ src/app/models/tecnico.ts
@@ -1,6 +1,6 @@
 export interface Tecnico {
     id?: any;
-    name: string;
+    nome: string;
     cpf: string;
     email: string;
     senha: string;
